945cd20c25a968fdf3f86754eef7ef18579fe2a7
mc08 <118325498+splitlane@users.noreply.github.com>
2024-10-18 17:15:02 -0700

    test chassis code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoWheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoWheel.java
@@ -1,35 +1,35 @@
-package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-
-@TeleOp
-public class TwoWheel extends LinearOpMode {
-    @Override
-    public void runOpMode() throws InterruptedException {
-        DcMotor leftMotor = hardwareMap.dcMotor.get("left");
-        DcMotor rightMotor = hardwareMap.dcMotor.get("right");
-
-        waitForStart();
-
-        if (isStopRequested()) return;
-
-        while (opModeIsActive()) {
-            double rx = gamepad1.right_trigger - gamepad1.left_trigger;
-            if (rx == 0) {
-                leftMotor.setPower(gamepad1.left_stick_y);
-                rightMotor.setPower(gamepad1.left_stick_y);
-            } else {
-                leftMotor.setPower(-rx);
-                rightMotor.setPower(rx);
-            }
-
-//            leftMotor.setPower(-gamepad1.left_trigger);
-//            rightMotor.setPower(-gamepad1.right_trigger);
-//            leftMotor.setPower(1);
-//            rightMotor.setPower(0);
-//            2 weeks cad, 1 week shipping 2 weeks build (for hardware), business 4 week port)
-        }
-    }
-}
+//package org.firstinspires.ftc.teamcode;
+//
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//import com.qualcomm.robotcore.hardware.DcMotor;
+//
+//@TeleOp
+//public class TwoWheel extends LinearOpMode {
+//    @Override
+//    public void runOpMode() throws InterruptedException {
+//        DcMotor leftMotor = hardwareMap.dcMotor.get("left");
+//        DcMotor rightMotor = hardwareMap.dcMotor.get("right");
+//
+//        waitForStart();
+//
+//        if (isStopRequested()) return;
+//
+//        while (opModeIsActive()) {
+//            double rx = gamepad1.right_trigger - gamepad1.left_trigger;
+//            if (rx == 0) {
+//                leftMotor.setPower(gamepad1.left_stick_y);
+//                rightMotor.setPower(gamepad1.left_stick_y);
+//            } else {
+//                leftMotor.setPower(-rx);
+//                rightMotor.setPower(rx);
+//            }
+//
+////            leftMotor.setPower(-gamepad1.left_trigger);
+////            rightMotor.setPower(-gamepad1.right_trigger);
+////            leftMotor.setPower(1);
+////            rightMotor.setPower(0);
+////            2 weeks cad, 1 week shipping 2 weeks build (for hardware), business 4 week port)
+//        }
+//    }
+//}
